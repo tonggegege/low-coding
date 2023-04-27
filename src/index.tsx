bc11d34471cc14323df04f6f361e57dae4363c1a
liguantong <codertg@163.com>
2023-04-28 00:21:12 +0800

    111测试

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+
+
+// 1111
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
