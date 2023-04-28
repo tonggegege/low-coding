409622e5bd3c1cee643af64d16d559e51b372147
liguantong <codertg@163.com>
2023-04-28 10:52:01 +0800

    222测试

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-
-
 // 1111
+// 222
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
