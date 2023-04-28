b59ea441f1d51dfbbdc5cef4b6948d6b935914d4
liguantong <codertg@163.com>
2023-04-28 11:19:46 +0800

    feat: 555提交

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 
-
-
+// 5555
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
