d639d63ffb77726b4b03d105a624f7a885d252d8
liguantong <codertg@163.com>
2023-04-28 11:18:36 +0800

    feat: clear number

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-// 1111
-// 222
-// 3333
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
